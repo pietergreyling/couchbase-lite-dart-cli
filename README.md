aba899d4e4ad012faac916819cd58b8bc51cc8ae
Pieter Greyling <pieter.greyling@gmail.com>
2022-05-30 19:43:57 +0100

    added REPL and database functions

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # couchbase-lite-dart-cli
-couchbase-lite-dart-cli
 
 Commandline interface Couchbase Lite utility built with the Dart programming language.
--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,5 +1,81 @@
-import 'package:cli/cli.dart' as cli;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${cli.calculate()}!');
+// import 'package:cli/cli.dart' as cli;
+import 'package:cli_repl/cli_repl.dart';
+import 'package:cbl/cbl.dart';
+import 'package:cbl_dart/cbl_dart.dart';
+
+late final Database db;
+
+main(List<String> args) async {
+  Repl repl = Repl(prompt: '>>> ', continuation: '... ', validator: validator);
+
+  // printStringList(args);
+
+  if (args.isNotEmpty) {
+    print("-- opening database: ${args[0]}");
+  }
+
+  await for (var x in repl.runAsync()) {
+    if (x.trim().isEmpty) continue;
+    if (x == 'throw;') throw "-- Oh no!";
+    if (x == 'exit;') throw "-- Bye bye";
+    if (x == 'quit;') throw "-- Quiting!";
+    print(x);
+  }
+}
+
+bool validator(String str) {
+  return str.trim().isEmpty || str.trim().endsWith(';');
+}
+
+void printStringList(List<String> list) {
+  print(list);
+}
+
+Future<Database> openDatabase(String dbname) async {
+  // await CouchbaseLiteDart.init(edition: Edition.community);
+  await CouchbaseLiteDart.init(edition: Edition.enterprise);
+  Database db = await Database.openAsync(dbname);
+  return db;
+}
+
+Future<void> closeDatabase(Database db) async {
+  await db.close();
+}
+
+Future<void> saveDocument(Database db, String doctext) async {
+  final doc = MutableDocument({
+    'type': 'text',
+    'createdAt': DateTime.now(),
+    'body': doctext,
+  });
+
+  await db.saveDocument(doc);
+
+  print('Document ${doc.id} stored: ${doc.toJson()}');
+}
+
+Future<void> listDocuments(Database db) async {
+  final query = const QueryBuilder()
+      .select(
+        SelectResult.expression(Meta.id),
+        SelectResult.property('createdAt'),
+        SelectResult.property('body'),
+      )
+      .from(DataSource.database(db))
+      .where(Expression.property('type').equalTo(
+        Expression.string('text'),
+      ))
+      .orderBy(Ordering.property('createdAt').descending());
+
+  final resultSet = await query.execute();
+  int documentCount = 0;
+
+  await for (final result in resultSet.asStream()) {
+    documentCount++;
+    print(result.toJson());
+  }
+
+  print('$documentCount documents found');
 }
